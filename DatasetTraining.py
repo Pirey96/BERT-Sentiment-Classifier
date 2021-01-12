1bc0c3ec3159a39cbdee6f72b8eddea6ce3d7e48
Pirey96 <pirey8888@gmail.com>
2021-01-11 20:19:13 -0500

    Training update

--- DatasetTraining.py
+++ DatasetTraining.py
@@ -1,4 +1,16 @@
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from transformers import BertForSequenceClassification,AdamW,BertConfig
+
+
 class DatasetTraining:
     print ("LOL")
     def __init__(self):
         print("Train with dataset")
+
+    def train_model(self):
+        model = BertForSequenceClassification.from_pretrained(
+            "bert-base-uncased",
+            num_label=16,
+            output_attentions=False,
+            output_loading_info=False,
+        )
